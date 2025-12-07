2528707cc45fe19a5454edf4eb340960c0fcf039
TouristOmer <omerfh025@gmail.com>
2025-12-07 18:23:24 +0300

    Update ex06.c
    Ömer F. Han

--- ex06.c
+++ ex06.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[17]; 
+    int index = 0;
+
+    for (int i = 99; i >= 50; i -= 3) {
+        numbers[index] = i;
+        index++;
+    }
+
+    printf("Output: ");
+    for (int j = 0; j < index; j++) {
+        printf("%d", numbers[j]);
+        
+        if (j < index - 1) {
+            printf(", ");
+        }
+    }
+    
+    return 0;
+}
